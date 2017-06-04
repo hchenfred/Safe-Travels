3837f90ee483d04fb42ff703a2b4768a5ae67adf
Huan Chen <hchen.fred@gmail.com>
2017-06-04 13:57:26 -0700

    (feat)config mlab

--- database/index.js
+++ database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/safeTravel');
+//mongoose.connect('mongodb://localhost/safeTravel');
+mongoose.connect('mongodb://heroku_wrrqkjq6:j3jkn9qp4l1oin1k6n817vl94v@ds161931.mlab.com:61931/heroku_wrrqkjq6');
 
 const db = mongoose.connection;
 
